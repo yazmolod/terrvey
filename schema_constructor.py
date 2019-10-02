2f5c616e43ac50763e06373625ee42d4efb3733a
Yazmo Work <yazmolod@gmail.com>
2019-10-02 11:32:47 +0400

    Constructor: import/export implemented

--- schema_constructor.py
+++ schema_constructor.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 import sys
+import json
 
 
 class SchemaBrick(QWidget):
@@ -12,12 +13,12 @@ class SchemaBrick(QWidget):
 
     def loadUI(self):
         self.questionTypes = [
-        ('Текст (строка)', 'text'),
-        ('Текст (абзац)', 'mtext'),
-        ('Один из списка', 'opt'),
-        ('Несколько из списка', 'mopt'),
-        ('Матрица с одним выбором', 'matrix'),
-        ('Матрица с несколькими выборами', 'mmatrix')
+            ('Текст (строка)', 'text'),
+            ('Текст (абзац)', 'mtext'),
+            ('Один из списка', 'opt'),
+            ('Несколько из списка', 'mopt'),
+            ('Матрица с одним выбором', 'matrix'),
+            ('Матрица с несколькими выборами', 'mmatrix')
         ]
         self.answers = []
         self.columns = []
@@ -58,19 +59,20 @@ class SchemaBrick(QWidget):
 
         self.questionTypeComboBox.currentIndexChanged.connect(self.typeChanged)
         self.answersCountSpinBox.valueChanged.connect(self.answersChanged)
-        self.matrixColumnsCountSpinBox.valueChanged.connect(self.columnsChanged)
-        self.tableNameLineEdit.textEdited.connect(self.tableNameEdited)
+        self.matrixColumnsCountSpinBox.valueChanged.connect(
+            self.columnsChanged)
+        self.tableNameLineEdit.textChanged.connect(self.tableNameChanged)
 
-    def tableNameEdited(self, text):
+    def tableNameChanged(self, text):
         stackedWidget = self.parent()
-        i = stackedWidget.currentIndex()
+        i = stackedWidget.count()-1
         stackedWidget.parent().setTabText(i, text)
 
     def answersChanged(self, value):
         if value > len(self.answers):
             while len(self.answers) != value:
                 le = QLineEdit()
-                self.layout.addWidget(le, len(self.answers) + 1, 2)
+                self.layout.addWidget(le, len(self.answers) + 2, 2)
                 self.answers.append(le)
         else:
             while len(self.answers) != value:
@@ -81,7 +83,7 @@ class SchemaBrick(QWidget):
         if value > len(self.columns):
             while len(self.columns) != value:
                 le = QLineEdit()
-                self.layout.addWidget(le, len(self.columns) + 1, 3)
+                self.layout.addWidget(le, len(self.columns) + 2, 3)
                 self.columns.append(le)
         else:
             while len(self.columns) != value:
@@ -111,22 +113,40 @@ class SchemaBrick(QWidget):
         quest = self.questionTitleLineEdit.text()
         table_name = self.tableNameLineEdit.text()
         table_column = self.tableColumnLineEdit.text()
-        csv_column = int(self.csvColumnSpinBox.text()) - 1        
-        type_text = self.questionTypes[self.questionTypeComboBox.currentIndex()][1]
+        csv_column = int(self.csvColumnSpinBox.text()) - 1
+        type_text = self.questionTypes[self.questionTypeComboBox.currentIndex(
+        )][1]
         type_bool = self.otherAnswerCheckBox.isChecked()
         if type_bool:
             type_text += '+'
         values = {
-        'answers': answers,
-        'answer_options': answer_options,
-        'quest': quest,
-        'table_name': table_name,
-        'type': type_text,
-        'csv_column': csv_column,
-        'table_column': table_column,
+            'answers': answers,
+            'answer_options': answer_options,
+            'quest': quest,
+            'table_name': table_name,
+            'type': type_text,
+            'csv_column': csv_column,
+            'table_column': table_column,
         }
         return values
 
+    def setValues(self, values):
+        for i in self.questionTypes:
+            if i[1]==values['type'].strip('+'):
+                current_type = i[0]  
+        index = self.questionTypeComboBox.findText(current_type)
+        self.questionTypeComboBox.setCurrentIndex(index)
+        self.answersCountSpinBox.setValue(len(values['answers']))
+        self.matrixColumnsCountSpinBox.setValue(len(values['answer_options']))
+        self.csvColumnSpinBox.setValue(values['csv_column'] + 1)
+        self.questionTitleLineEdit.setText(values['quest'])
+        self.tableNameLineEdit.setText(values['table_name'])
+        self.tableColumnLineEdit.setText(values['table_column'])
+        self.otherAnswerCheckBox.setChecked('+' in values['type'])
+        for iAnswer in range(len(values['answers'])):
+            self.answers[iAnswer].setText(values['answers'][iAnswer])
+        for iAnswerOption in range(len(values['answer_options'])):
+            self.columns[iAnswerOption].setText(values['answer_options'][iAnswerOption])
 
 class SchemaConstructor(QWidget):
     def __init__(self):
@@ -137,21 +157,40 @@ class SchemaConstructor(QWidget):
     def initSignals(self):
         self.addButton.clicked.connect(self.addQuestion)
         self.removeButton.clicked.connect(self.removeQuestion)
-        self.exportButton.clicked.connect(self.export)
+        self.exportButton.clicked.connect(self.exportProject)
+        self.importButton.clicked.connect(self.importProject)
 
     def addQuestion(self):
         wid = SchemaBrick()
         self.tabWidget.addTab(wid, '?')
+        return wid
 
     def removeQuestion(self):
         self.tabWidget.removeTab(self.tabWidget.currentIndex())
 
-    def export(self):
+    def exportProject(self):
         schema = []
         for i in range(len(self.tabWidget)):
             wid = self.tabWidget.widget(i)
-            schema.append (wid.getValues())
-        print (schema)
+            schema.append(wid.getValues())
+        fname = QFileDialog.getSaveFileName(
+            self, 'Создать проект опроса', '.', "База данных (*.json)")[0]
+        if fname:
+            with open(fname, 'w', encoding='utf-8') as file:
+                json.dump(schema, file, ensure_ascii=False)
+
+    def importProject(self):
+        fname = QFileDialog.getOpenFileName(
+            self, 'Открыть проект опроса', '.', "База данных (*.json)")[0]
+        if fname:
+            with open(fname, 'r', encoding='utf-8') as file:
+                schema = json.load(file)
+            self.tabWidget.clear()
+            for q in schema:
+                wid = self.addQuestion()
+                wid.setValues(q)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
